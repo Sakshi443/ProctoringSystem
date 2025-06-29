5ab95068c1245b0449178c51931a924ae4195c94
Sakshi Manoorkar <126005443+Sakshi443@users.noreply.github.com>
2025-06-29 21:58:04 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Serve static files (optional)
-app.use(express.static(path.join(__dirname, 'templates')));
+app.use(express.static(path.join(__dirname, "templates")));
 
 // Serve HTML template from "templates"
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates", "index.html"));
 });
+
+app.use("/models", express.static(path.join(__dirname, "models")));
 
 // Catch-all route for 404 errors
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'templates', 'error.html'));
+  res.status(404).sendFile(path.join(__dirname, "templates", "error.html"));
 });
 
 app.listen(PORT, () => {
